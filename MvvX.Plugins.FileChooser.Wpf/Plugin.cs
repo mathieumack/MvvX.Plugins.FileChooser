78b5f17e48906aa706e22e10cf1ef42e41d5b95c
ChebMami38 <vincent.daude@econocom.com>
2016-12-27 14:27:29 +0100

    Add Plugin and PluginLoader for MvvmCross plugin management

---
+++ MvvX.Plugins.FileChooser.Wpf/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.Platform;
+using MvvmCross.Platform.Plugins;
+
+namespace MvvX.Plugins.FileChooser.Wpf
+{
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType<IFileChooser>(() =>
+            {
+                return new WpfFileChooser();
+            });
+        }
+    }
+}
